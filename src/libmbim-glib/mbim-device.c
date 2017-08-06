15a42d0f0480fe86ee61415660b27f9cc5c3e02d
Aleksander Morgado <aleksander@aleksander.es>
2017-08-06 13:13:40 +0200

    device: avoid signals sent to the mbim-proxy process
    If e.g. mbim-proxy is started by ModemManager and we send a Ctrl+C to
    it, the signal would be propagated to the mbim-proxy process and we
    would kill it right away, while leaving ModemManager still around
    wondering why the socket to the proxy got a HUP.
    
    Avoid this, by making sure the mbim-proxy gets its own process group.
    
    (cherry picked from commit 5bb673fe87ff9462bdec6435487d57982f886968)

--- src/libmbim-glib/mbim-device.c
+++ src/libmbim-glib/mbim-device.c
@@ -1069,6 +1069,13 @@ wait_for_proxy_cb (CreateIoChannelContext *ctx)
 }
 
 static void
+spawn_child_setup (void)
+{
+    if (setpgid (0, 0) < 0)
+        g_warning ("couldn't setup proxy specific process group");
+}
+
+static void
 create_iochannel_with_socket (CreateIoChannelContext *ctx)
 {
     GSocketAddress *socket_address;
@@ -1125,7 +1132,7 @@ create_iochannel_with_socket (CreateIoChannelContext *ctx)
                             argc,
                             NULL, /* envp */
                             G_SPAWN_STDOUT_TO_DEV_NULL | G_SPAWN_STDERR_TO_DEV_NULL,
-                            NULL, /* child_setup */
+                            (GSpawnChildSetupFunc) spawn_child_setup,
                             NULL, /* child_setup_user_data */
                             NULL,
                             &error)) {
