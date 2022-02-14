d08cbcfe160f6cafa54d9e50a3975e6cb5b97f9a
Aleksander Morgado <aleksander@aleksander.es>
2022-02-14 22:30:42 +0100

    libmbim-glib,compat: add missing MbimLteAttachStatus doc
    (cherry picked from commit c70d602cd4dbe2e874eb6efd036bf7e523976052)

--- src/libmbim-glib/mbim-compat.h
+++ src/libmbim-glib/mbim-compat.h
@@ -145,6 +145,8 @@ typedef int MbimDeprecatedCidMsBasicConnectExtensions;
  * @compression: a #guint32.
  * @auth_protocol: a #guint32.
  *
+ * LTE attach status information.
+ *
  * Since: 1.18
  * Deprecated: 1.26
  */
